42f26f720110e5ecf5d364731c09773a9142ad0c
Yehor <quick.man.qm@gmail.com>
2023-02-12 09:46:09 +0200

    first commit

---
+++ LaboratoryWork7/LaboratoryWork7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaboratoryWork7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
