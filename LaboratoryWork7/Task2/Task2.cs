141eda4524670a83cf3c9f141ae19efaba215c58
Yehor <quick.man.qm@gmail.com>
2023-02-16 15:11:10 +0200

    Ending Task2

--- LaboratoryWork7/Task2/Task2.cs
+++ LaboratoryWork7/Task2/Task2.cs
@@ -4,9 +4,37 @@ namespace Task2
 {
     internal class Task2
     {
+        public delegate int MyDel();
+
+        public static int Methods(MyDel[] array)
+        {
+            int x = 0;
+            foreach (MyDel del in array)
+            {
+                x += del();
+            }
+            x /= array.Length;
+            Console.WriteLine(x);
+            return x;
+        }
+
+        public int ForArray()
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(1,10);
+            Console.WriteLine(x);
+            return x;
+        }
+
         static void Main(string[] args)
         {
-            
+            Task2 task = new Task2();
+            MyDel del = task.ForArray;
+            MyDel del2 = task.ForArray;
+            MyDel[] dels = new MyDel[2];
+            dels[0] = del;
+            dels[1] = del2;
+            Methods(dels);
         }
     }
 }
