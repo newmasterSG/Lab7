fd32fc846889e4089afb76d163694523d4fc6877
Yehor <quick.man.qm@gmail.com>
2023-02-16 08:55:02 +0200

    Ending Task1

---
+++ LaboratoryWork7/LaboratoryWork7/Task1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LaboratoryWork7
+{
+    internal class Task1
+    {
+        public delegate int arithmetic(int x, int y);
+
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            int p = int.Parse(Console.ReadLine());
+            arithmetic arithmetic = (x, y) => x + y;
+            Console.WriteLine(arithmetic(t, p));
+            arithmetic += (x, y) => x - y;
+            Console.WriteLine(arithmetic(t, p));
+            arithmetic += (x, y) => x * y;
+            Console.WriteLine(arithmetic(t, p));
+            arithmetic += (x, y) =>
+            {
+               int g = 0;
+               try
+               {
+                  g = x / y;
+               }
+               catch(DivideByZeroException e)
+               {
+                  Console.WriteLine(e.Message);
+               }
+               return g;
+            };
+            Console.WriteLine(arithmetic(t, p));
+        }
+    }
+}
---
+++ LaboratoryWork7/Task2/Task2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Task2
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
